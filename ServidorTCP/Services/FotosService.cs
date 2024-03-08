e23181547f0b3cac89c8d6d199f548706aae2f7a
JorgeFabian315 <jfabi@FABIAN>
2024-03-07 21:35:50 -0600

    .

--- ServidorTCP/Services/FotosService.cs
+++ ServidorTCP/Services/FotosService.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ServidorTCP.Models.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace ServidorTCP.Services
 {
@@ -12,7 +15,7 @@ namespace ServidorTCP.Services
     {
         TcpListener server = null!;
         List<TcpClient> clientes = new List<TcpClient>();
-        
+        public event EventHandler<FotoDto>? RecibirFotoEvent;
 
         public void Iniciar()
         {
@@ -20,7 +23,6 @@ namespace ServidorTCP.Services
             server.Start();
             new Thread(Escuchar) { IsBackground = true }.Start();
         }
-
         private void Escuchar(object? obj)
         {
             while(server.Server.IsBound)
@@ -30,16 +32,71 @@ namespace ServidorTCP.Services
 
                 new Thread(() =>
                 {
-                    RecibirMensajes(tcpClient);
+                    RecibirFotos(tcpClient);
                 })
-                { IsBackground = true};
-
+                { IsBackground = true}.Start();
             }
         }
 
-        private void RecibirMensajes(TcpClient tcpClient)
+        private void RecibirFotos(TcpClient tcpClient)
         {
-            throw new NotImplementedException();
+            while (tcpClient.Connected)
+            {
+                var ns = tcpClient.GetStream();
+
+                while(tcpClient.Available == 0)
+                    Thread.Sleep(500);
+
+                var buffer = new byte[tcpClient.Available];
+                ns.Read(buffer, 0, buffer.Length);
+
+                var json = Encoding.UTF8.GetString(buffer);
+
+                var foto = JsonSerializer.Deserialize<FotoDto>(json);
+
+                if(foto != null)
+                {
+                    RelayFotos(tcpClient, buffer);
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        RecibirFotoEvent?.Invoke(this, foto);
+                    });
+                }
+
+            }
+            clientes.Remove(tcpClient);
+        }
+
+        private void RelayFotos(TcpClient tcpClient, byte[] buffer)
+        {
+            foreach (var item in clientes)
+            {
+                if (item != tcpClient)//Enviar a todos menos al origen
+                {
+                    var ns = item.GetStream();
+                    ns.Write(buffer, 0, buffer.Length);
+                    ns.Flush();
+                }
+            }
+        }
+        public void Detener()
+        {
+            try
+            {
+                if (server != null)
+                {
+                    server.Stop();
+                    foreach (var item in clientes)
+                    {
+                        item.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Agregar excepcion
+            }
         }
     }
 }
